c8fa5abc7a04d1bf24f119adc70062dbb3819c68
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-07-22 12:09:09 -0400

    Modified to use CSV

--- BarPlugin.R
+++ BarPlugin.R
@@ -1,5 +1,6 @@
+library(microbiome)
 library(ggplot2)
-library(phyloseq)
+#library(phyloseq)
 library(ape)
 library(psadd)
 
@@ -7,22 +8,25 @@ input <- function(inputfile) {
   parameters <<- read.table(inputfile, as.is=T);
   rownames(parameters) <<- parameters[,1]; 
    # Need to get the three files
-   otu.path <- parameters["otufile", 2]
-   tree.path <- parameters["tree", 2]
-   map.path <- parameters["mapping", 2]
+   otu.path <<- parameters["otufile", 2]
+   tree.path <<- parameters["tree", 2]
+   map.path <<- parameters["mapping", 2]
    diffcol <<- parameters["column", 2]
-   HMP <<- import_qiime(otu.path, map.path, tree.path, parseFunction = parse_taxonomy_qiime)
+   #HMP <<- import_qiime(otu.path, map.path, tree.path, parseFunction = parse_taxonomy_qiime)
 }
 run <- function() {
-   samples.to.keep <<- sample_sums(HMP) >= 1000
-   HMP <<- prune_samples(samples.to.keep, HMP)
-   HMP <<- filter_taxa(HMP, function(x) sum(x >3) > (0.01*length(x)), TRUE)
+   #samples.to.keep <<- sample_sums(HMP) >= 1000
+   #HMP <<- prune_samples(samples.to.keep, HMP)
+   #HMP <<- filter_taxa(HMP, function(x) sum(x >3) > (0.01*length(x)), TRUE)
+   p0 <<- read_csv2phyloseq(otu.file=otu.path, taxonomy.file=tree.path, metadata.file=map.path)
 }
 output <- function(outputfile) {
   pdf(paste(outputfile,"pdf",sep="."))#,  width = 10*300,        # 5 x 300 pixels
   #height = 10*300); #,)
-  y <- plot_bar(HMP, x="Description", fill=diffcol)
+  print("Generating plot...")
+  y <- plot_bar(p0, x="Sample", fill=diffcol)
   #print(str(y))
+  print("Generating CSV...")
   #print(str(y$data))
   write.csv(y$data, paste(outputfile,"csv",sep="."))
   print(y)#plot_bar(HMP, x="Description", fill=diffcol))
